644edcb01d54f72afc5edce80bcd1ad1dc2d0aa9
weir0062 <weir0062@algonquinlive.com>
2023-03-30 21:05:35 -0400

    added ChatGPT

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const db = firebase.firestore();
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-Drdsjm1gOMAanaw4foPs6GvV",
+    apiKey: process.env.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,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
+
+
+
 const chatForm = document.getElementById('chat-form');
 const chatInput = document.getElementById('chat-input');
 const chatBody = document.getElementById('chat-body');
@@ -16,14 +26,30 @@ function addMessage(sender, message) {
   scrollToBottom();
 }
 
-// Fetch chatbot response
-async function getChatbotResponse(message) {
-  // Replace this function with an API call or custom logic to get a chatbot response
-  return `You said: ${message}`;
+// Simple rule-based chatbot logic
+function getChatbotResponse(message) {
+  message = message.toLowerCase();
+  const completion =  openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: message.textContent}],
+  });
+  return completion.data.choices[0].message;
+
+
+
+ /* if (message.includes('hello') || message.includes('hi')) {
+    return 'Hello! How can I help you?';
+  } else if (message.includes('how are you')) {
+    return "I'm doing well, thank you! How can I assist you today?";
+  } else if (message.includes('your name')) {
+    return "I'm a simple rule-based chatbot!";
+  } else {
+    return "I'm not sure how to respond to that. Please try asking something else!";
+  }*/
 }
 
 // Listen for form submission
-chatForm.addEventListener('submit', async (e) => {
+chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const userMessage = chatInput.value.trim();
@@ -32,7 +58,7 @@ chatForm.addEventListener('submit', async (e) => {
   addMessage('You', userMessage);
 
   // Get chatbot response
-  const chatbotResponse = await getChatbotResponse(userMessage);
+  const chatbotResponse = getChatbotResponse(userMessage);
   addMessage('Chatbot', chatbotResponse);
 
   // Clear input field
